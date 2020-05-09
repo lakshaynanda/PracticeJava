9b48138fbe4e56e480d547d6431c9dc129962879
lakshaynanda <lakshay.nanda08@gmail.com>
2020-05-09 10:18:21 +0530

    Updated list

--- L03/shortestHeight.java
+++ L03/shortestHeight.java
@@ -3,7 +3,7 @@ public class shortestHeight {
     public static int shortestheightTree(int sr, int sc, int er, int ec, int board[][]) {
 
         int dirA[][] = { { 0, -1 }, { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, 1 }, { 1, 1 }, { 1, 0 }, { 1, -1 } };
-
+        String[] dirS = { "L", "N", "U", "E", "R", "S", "D", "W" };
         if (sr == er && sc == ec) {
             return 0;
         }
@@ -24,13 +24,53 @@ public class shortestHeight {
         board[sr][sc] = 0;
         return myHeight + 1;
     }
+    static class pair{
+        String path;
+        int len;
+        pair(String path_,int len_)
+        {
+            path=path_;
+            len=len_;
+        }
+    }
+
+    public static pair shortestHeightTree(int sr,int sc,int er,int ec,int[][] board)
+    {
+        int dirA[][] = { { 0, -1 }, { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, 1 }, { 1, 1 }, { 1, 0 }, { 1, -1 } };
+        String[] dirS = { "L", "N", "U", "E", "R", "S", "D", "W" };
+        if(sr==er && sc==ec)
+        {
+            return new pair("",0);
+        }
+        board[sr][sc]=1;
+        pair myAns=new pair("",board.length*board[0].length);
+
+        for(int d=0;d<dirA.length;d++)
+        {
+            int x = sr + dirA[d][0];
+            int y = sc + dirA[d][1];
+
+            if (x >= 0 && y >= 0 && x < board.length && y < board.length && board[x][y] == 0) {
+                pair smallAns=shortestHeightTree(x,y, er, ec, board);
+                if(smallAns + 1 < myAns.len)
+                {
+                    myAns.len=smallAns.len+1;
+                    myAns.path=dirS[d]+smallAns.path;
+                }
+                
+            }
+        }
+        board[sr][sc]=0;
+        return myAns;
+        
+    }
 
     public static void main(String[] args) {
 
-        String[] dirS = { "L", "N", "U", "E", "R", "S", "D", "W" };
+        
 
         int board[][] = new int[3][3];
-        System.out.println(shortestheightTree(0, 0, 2, 2, board));
+        System.out.println(shortestHeightTree(0, 0, 2, 2, board));
 
     }
 
